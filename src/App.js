912f76da60f2444635b927436d189a26d30e2ab0
Qandi <dltmdwo430@gmail.com>
2019-08-13 23:40:37 +0900

    added props on Customer.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
 
+
+const customer = {
+  'name':'홍길동',
+  "birthDay":"961222",
+  "gender":"남자",
+  "job":"대학생"
+}
+
 class App extends Component{
   render(){
     return(
-      <Customer/>
+      <Customer
+        name={customer.name}
+        birthDay={customer.birthDay}
+        gender={customer.gender}
+        job={customer.job}
+      />
     )
   }
 }
