71b3906a35dd0441c20041edb5dcaf865d81fcfe
Qandi <dltmdwo430@gmial.com>
2019-08-14 11:47:16 +0900

    send joson to app.js and binding data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
 import {Paper, TableRow,TableCell,Table, TableHead, TableBody } from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
+import { async } from 'rxjs/internal/scheduler/async';
 
 const styles = theme => ({
   root:{
@@ -16,34 +16,26 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    "id":1,
-    "image":"https://placeimg.com/64/64/any",
-    'name':'홍길동',
-    "birthDay":"961222",
-    "gender":"남자",
-    "job":"대학생"
-  },
-  {
-    "id":2,
-    "image":"https://placeimg.com/64/64/any",
-    'name':'이승재',
-    "birthDay":"900430",
-    "gender":"남자",
-    "job":"회사원"
-  },
-  {
-    "id":3,
-    "image":"https://placeimg.com/64/64/any",
-    'name':'이순신',
-    "birthDay":"961222",
-    "gender":"남자",
-    "job":"대학생"
-  }
-]
 
 class App extends Component{
+
+  state = {
+    customers:""  
+  }
+
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers:res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+
   render(){
     const {classes} = this.props;
     return(
@@ -60,9 +52,9 @@ class App extends Component{
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c =>{
+            {this.state.customers ? this.state.customers.map(c =>{
               return <Customer key={c.id} id={c.id} image={c.image} name ={c.name} birthDay={c.birthDay} gender={c.gender} job={c.job} />
-            })}
+            }): ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -9,4 +9,33 @@ app.get('/api/hello',(req, res) => {
     res.send({ message : "Hello Express!"});
 })
 
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        {
+          "id":1,
+          "image":"https://placeimg.com/64/64/any",
+          'name':'홍길동',
+          "birthDay":"961222",
+          "gender":"남자",
+          "job":"대학생"
+        },
+        {
+          "id":2,
+          "image":"https://placeimg.com/64/64/any",
+          'name':'이승재',
+          "birthDay":"900430",
+          "gender":"남자",
+          "job":"회사원"
+        },
+        {
+          "id":3,
+          "image":"https://placeimg.com/64/64/any",
+          'name':'이순신',
+          "birthDay":"961222",
+          "gender":"남자",
+          "job":"대학생"
+        }
+      ])
+})
+
 app.listen(port, () => console.log(`Listening on ${port}`));
