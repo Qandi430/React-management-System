fada0963366a14d9b6f81f8119a98022276c49c3
Qandi <dltmdwo430@gmial.com>
2019-08-14 18:12:59 +0900

    create CustomerAdd.js for insert customer data

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Customer from './components/Customer';
 import {Paper, TableRow,TableCell,Table, TableHead, TableBody, CircularProgress } from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
 import { async } from 'rxjs/internal/scheduler/async';
+import CustomerAdd from './components/CustomerAdd';
 
 const styles = theme => ({
   root:{
@@ -53,32 +54,35 @@ class App extends Component{
   render(){
     const {classes} = this.props;
     return(
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? this.state.customers.map(c =>{
-              return <Customer key={c.ID} id={c.ID} image={c.IMAGE} name ={c.NAME} birthDay={c.BIRTHDAY} gender={c.GENDER} job={c.JOB} />
-            }):
-            <TableRow>
-              <TableCell colSpan="6" align="center">
-                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-              </TableCell>
-            </TableRow>
-            }
-            
-          </TableBody>
-        </Table>
-      </Paper>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? this.state.customers.map(c =>{
+                return <Customer key={c.ID} id={c.ID} image={c.IMAGE} name ={c.NAME} birthDay={c.BIRTHDAY} gender={c.GENDER} job={c.JOB} />
+              }):
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              }
+              
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd/>
+      </div>
     )
   }
 }
