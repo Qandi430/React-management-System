0be4fa5337e773cae74722b46e005e1eded5c6d3
Qandi <dltmdwo430@gmial.com>
2019-08-14 11:00:45 +0900

    changed ui with material-ui

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
+import {Paper, TableRow,TableCell,Table, TableHead, TableBody } from '@material-ui/core';
+import {withStyles} from '@material-ui/core/styles';
 
+const styles = theme => ({
+  root:{
+    width:"100%",
+    marginTop:theme.spacing.unit*3,
+    overflowX:"auto"
+  },
+  table:{
+    minWidth:1080
+  }
+})
 
 const customers = [
   {
@@ -33,14 +45,29 @@ const customers = [
 
 class App extends Component{
   render(){
+    const {classes} = this.props;
     return(
-      <div>
-        {customers.map(c =>{
-          return <Customer key={c.id} id={c.id} image={c.image} name ={c.name} birthDay={c.birthDay} gender={c.gender} job={c.job} />
-        })}
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c =>{
+              return <Customer key={c.id} id={c.id} image={c.image} name ={c.name} birthDay={c.birthDay} gender={c.gender} job={c.job} />
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
